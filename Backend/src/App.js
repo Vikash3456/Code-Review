b822612ab928943a9974ba3e1d4195c7e1975d2f
vikash kushwaha <vikashkush29@gmail.com>
2025-04-11 01:38:15 +0530

    finaly completed

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const aiRoutes=require('./routes/ai.routes')
- const app=express() //  create server instance  and run on port 3000  [if we use only express() it just create server instance ]
+const app=express() //  create server instance  and run on port 3000  [if we use only express() it just create server instance ]
+const cors=require('cors')
 
 
-
+app.use(cors())
+app.use(express.json())
 
  app.get('/',(req,res) =>{
     res.send('hello world')
  } )
 
-app.use(express.json())
 app.use('/ai',aiRoutes)
+
 
 
 module.exports=app;
